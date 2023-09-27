d818e00655f320efe9426c8bcfae47f7ff271664
Raziraptor <144977386+Raziraptor@users.noreply.github.com>
2023-09-27 16:16:47 -0600

    Se creo el repositorio y se agregaron los archivos

---
+++ src/Card.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Card {
+    private String palo;
+    private String color;
+    private String valor;
+
+    public Card(String palo, String color, String valor) {
+        this.palo = palo;
+        this.color = color;
+        this.valor = valor;
+    }
+
+    public String getPalo() {
+        return palo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    @Override
+    public String toString() {
+        return palo + "," + color + "," + valor;
+    }
+}
